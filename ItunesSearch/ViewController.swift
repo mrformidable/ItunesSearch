86b37b0033095e9e13d68eec723ad84f016b16a9
Michael Aidoo <michael@michaels-macbook-pro.local>
2018-07-29 21:08:40 -0400

    Initial Commit

---
+++ ItunesSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItunesSearch
+//
+//  Created by Michael Aidoo on 2018-07-29.
+//  Copyright © 2018 Michael Aidoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
